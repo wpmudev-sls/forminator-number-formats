dc0564538baacfd7f327995a3b3f19e0535139cf
wpmudev-sls <30955326+wpmudev-sls@users.noreply.github.com>
2019-07-31 20:01:40 +0300

    Update forminator-number-formats.php

--- forminator-number-formats.php
+++ forminator-number-formats.php
@@ -4,7 +4,7 @@
  * Version: 1.0.0
  * Plugin URI:  https://premium.wpmudev.org/project/forminator/
  * Description: Adds number formats to Forminator Input fields. It currently can be used only for Forminator's Input fields (normal textboxes), so it won't work with Number, Phone or other fields. Uses https://nosir.github.io/cleave.js/
- * Author: WPMU DEV
+ * Author: Panos Lyrakis @ WPMUDEV
  * Author URI: http://premium.wpmudev.org
  */
 
